ed866bd39d0b53f4d3c8b78a1bb1225715f166ce
marefpceo <32721274+marefpceo@users.noreply.github.com>
2024-09-16 10:29:02 -0700

    Setup and created views
    Created partial views for Head, Header and Footer.
    Updated index page to display title, File Uploader.
    Created views and routes for login and signup.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+// GET login page
+router.get('/login', (req, res, next) => {
+  res.render('login', {
+    title: 'Login ',
+  });
 });
+
+// GET home page
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'File Uploader' });
+});
+
+// GET sign up page
+router.get('/signup', (req, res, next) => {
+  res.render('signup', {
+    title: 'Sign Up'
+  });
+});
+
 
 module.exports = router;
