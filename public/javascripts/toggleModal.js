a051d9e10ca54f518557a5891de5766a87bd4e4e
marefpceo <32721274+marefpceo@users.noreply.github.com>
2024-10-04 15:56:54 -0700

    Completed file info file modal for mobile view

--- public/javascripts/toggleModal.js
+++ public/javascripts/toggleModal.js
@@ -1,10 +1,33 @@
-let fileModalDisplay = document.getElementById('file-modal');
+
   
 // Show or hide file modal
-function toggleModal() {
-  if (fileModalDisplay.style.display == 'none') {
+function toggleModal(file) {
+  let fileModalDisplay = document.getElementById('file-modal');
+  
+  if (!fileModalDisplay.style.display || fileModalDisplay.style.display == 'none') {
     fileModalDisplay.style.display = 'flex';
+    document.getElementById('content-header').innerHTML = file.filename;
+    document.getElementById('modal-content-body').innerHTML = modalBodyOutput(file.last_modified, 
+      file.file_size);
+    document.getElementById('modal-content-footer').innerHTML = modalFooterOutput(file.id);
   } else {
     fileModalDisplay.style.display = 'none';
   }
+}
+
+function modalBodyOutput(modified, size) {
+  const htmlOutput = `
+    <p><strong>Last modified:</strong> <br>${modified}</p>
+    <p><strong>Size: </strong> <br>${size}</p>
+  `
+  return htmlOutput;
+}
+
+function modalFooterOutput(fileId) {
+  const htmlOutput = `
+      <a href='/file/${fileId}'><img src='/images/download-icon.png' alt='Download file'></a>
+      <a href='/file/${fileId}/edit'><img src='/images/edit-icon.png' alt='Edit file name'></a>
+      <a href='/file/${fileId}/delete'><img src='/images/delete-icon.png' alt='Delete file'></a>
+  `
+  return htmlOutput;
 }
